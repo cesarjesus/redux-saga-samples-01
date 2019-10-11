b8ceb15d1eff72a81aedcf520d7a12af1e9591ca
Cesar Flores <cesar.flores@truextend.com>
2019-10-11 19:10:17 -0400

    And more progress, just to keep files safe and clone in another env.

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,10 +1,19 @@
 import React from "react";
 
 class Counter extends React.Component {
+  handleOnIncrease() {
+    console.info("On Increase");
+  }
+
+  handleOnDecrease() {
+    console.info("On Decrease");
+  }
+
   render() {
     return (
       <div>
-        <button>Increase</button> | <button>Decrease</button>
+        <button onClick={this.handleOnIncrease}>Increase</button>|
+        <button onClick={this.handleOnDecrease}>Decrease</button>
       </div>
     );
   }
